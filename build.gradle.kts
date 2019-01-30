c0dd5fc113252f2b2940db4ecf6b213d4e864aaa
Joseph A. Kratz <joe.kratz@chase.com>
2019-01-29 21:36:53 -0500

    Bintray Publish working

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,22 @@
+import com.jfrog.bintray.gradle.BintrayExtension
+import org.jetbrains.dokka.gradle.DokkaTask
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import java.util.Date
 
 plugins {
     kotlin("jvm") version "1.3.11"
+    maven
+    `maven-publish`
+    jacoco
+    id("com.jfrog.bintray") version "1.8.1"
+    id("org.jetbrains.dokka") version "0.9.17"
 }
 
 group = "io.jkratz.springmediatr"
-version = "1.0-SNAPSHOT"
+version = "1.0-RC"
 
 repositories {
-    mavenCentral()
+    jcenter()
 }
 
 dependencies {
@@ -23,4 +31,72 @@ dependencies {
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
+}
+
+val sourceJar = task("sourceJar", Jar::class) {
+    dependsOn(tasks["classes"])
+    classifier = "sources"
+    from(sourceSets.main.get().allSource)
+}
+
+val javadocJar = task("javadocJar", Jar::class) {
+    val javadoc = tasks["dokka"] as DokkaTask
+    javadoc.outputFormat = "javadoc"
+    javadoc.outputDirectory = "$buildDir/javadoc"
+    dependsOn(javadoc)
+    classifier = "javadoc"
+    from(javadoc.outputDirectory)
+}
+
+publishing {
+    publications {
+        create("mavenJava", MavenPublication::class.java).apply {
+            groupId = project.group.toString()
+            this.artifactId = artifactId
+            version = project.version.toString()
+            pom {
+                description.set("Implementation of Mediator pattern for JVM and Spring Framework")
+                name.set(artifactId)
+                url.set("https://github.com/jkratz55/spring-mediatR")
+                licenses {
+                    license {
+                        name.set("Apache License 2.0")
+                        url.set("https://opensource.org/licenses/Apache-2.0")
+                        distribution.set("repo")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("jkratz55")
+                        name.set("Joseph Kratz")
+                        email.set("joseph.kratz06@gmail.com")
+                    }
+                }
+                scm {
+                    url.set("https://github.com/jkratz55/spring-mediatR")
+                }
+            }
+
+            from(components["java"])
+            artifact(sourceJar)
+            artifact(javadocJar)
+        }
+    }
+}
+
+bintray {
+    user = System.getenv("BINTRAY_USER")
+    key = System.getenv("BINTRAY_API_KEY")
+    pkg(closureOf<BintrayExtension.PackageConfig> {
+        repo = "maven-public"
+        name = "spring-mediatR"
+        setLicenses("Apache-2.0")
+        vcsUrl = "https://github.com/jkratz55/spring-mediatR"
+        publish = true
+        setPublications("mavenJava")
+        version(closureOf<BintrayExtension.VersionConfig> {
+            this.name = project.version.toString()
+            released = Date().toString()
+        })
+    })
 }
